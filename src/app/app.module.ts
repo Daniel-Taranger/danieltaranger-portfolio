7d7485a79990abb64146e057c77093b476a0df65
BOUVET\daniel.taranger <danie.taranger@bouvet.no>
2018-02-12 15:15:00 +0100

    masse funksjonalitet og animasjon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { EntryComponent } from './entry/entry.component';
 import { MenuComponent } from './menu/menu.component';
-import { DataserviceComponent } from './dataservice/dataservice.component';
+import { DataService } from './dataservice/dataservice.component';
 
 
 @NgModule({
-  declarations: [AppComponent, EntryComponent, MenuComponent, DataserviceComponent],
+  declarations: [AppComponent, EntryComponent, MenuComponent],
   imports: [
     BrowserModule,
     HttpModule
   ],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,25 +1,35 @@
 import { Http} from '@angular/http';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../dataservice/dataservice.component';
 
 @Component({
   selector: 'entry-generator',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.scss']
 })
-export class EntryComponent {
-  data;
-    constructor(private http:Http) {
-        this.http.get('./assets/data.json')
-                .subscribe(res => this.data = res.json());
 
+export class EntryComponent {
+  data = [];
+
+  toggle = false;
+  toggleElements = [];
+
+  toggleSizeFull = "500px";
+  toggleSizeSmall= "250px";
+
+
+  constructor(private dataService: DataService) {
+    this.dataService.getData().subscribe(data => this.data = data);
+    this.data.sort(this.date_sort_desc);
   }
-  
+
+
   parseDates() {
     if(this.data != null ){      
       for (var i = 0, len = this.data.length; i < len; i++) {
         this.data[i].date = this.convert(this.data[i].date );
+        console.log(typeof this.data[i].date);
       }
-      this.data.sort(this.date_sort_desc);
     }      
   }
   
@@ -34,31 +44,60 @@ export class EntryComponent {
   date_sort_desc = function (date1, date2) {
     // This is a comparison function that will result in dates being sorted in
     // DESCENDING order.
-    if (date1 > date2) return -1;
-    if (date1 < date2) return 1;
+    if (date1.date > date2.date) return -1;
+    if (date1.date < date2.date) return 1;
     return 0;
   }
   
   date_sort_asc= function (date1, date2) {
     // This is a comparison function that will result in dates being sorted in
-    // DESCENDING order.
-    if (date1 > date2) return 1;
-    if (date1 < date2) return -1;
+    // ASCENDING order.
+    if (date1.date < date2.date) return -1;
+    if (date1.date > date2.date) return 1;
     return 0;
   }
   
   sortData(sort) {
       if(sort == "oldest"){
         this.data.sort(this.date_sort_asc);
-        console.log("sorted by oldest" + this.data[1].date);
-        this.data[1].date;
       }else if(sort == "newest"){
         this.data.sort(this.date_sort_desc);
-        console.log("sorted by newest" + this.data[1].date);
       }else{
         console.log("error");
       }
     }
-  
-    
+
+    expandImage(event) {
+      var image = document.getElementById(event.target.id);
+
+      if(image.parentElement.style.height != this.toggleSizeFull){
+        for (var i = 0, len = this.toggleElements.length; i < len; i++) {
+          this.toggleElements[i].parentElement.style.height = this.toggleSizeSmall;
+          this.toggleElements[i].parentElement.style.width = this.toggleSizeSmall;
+          this.toggleElements[i].style.height = this.toggleSizeSmall;
+          this.toggleElements[i].style.width = this.toggleSizeSmall;
+          this.toggleElements.splice(0, 1);
+        }
+
+        image.parentElement.style.height = this.toggleSizeFull;
+        image.parentElement.style.width = this.toggleSizeFull;
+        image.style.height = this.toggleSizeFull;
+        image.style.width = this.toggleSizeFull;
+        this.toggleElements.push(image);
+      } else {
+        for (var i = 0, len = this.toggleElements.length; i < len; i++) {
+          this.toggleElements[i].parentElement.style.height = this.toggleSizeSmall;
+          this.toggleElements[i].parentElement.style.width = this.toggleSizeSmall;
+          this.toggleElements[i].style.height = this.toggleSizeSmall;
+          this.toggleElements[i].style.width = this.toggleSizeSmall;
+          this.toggleElements.splice(0, 1);
+        }
+
+        image.parentElement.style.height = this.toggleSizeSmall;
+        image.parentElement.style.width = this.toggleSizeSmall;
+        image.style.height = this.toggleSizeSmall;
+        image.style.width = this.toggleSizeSmall;
+        this.toggleElements.push(image);
+      }
+    }
 }
